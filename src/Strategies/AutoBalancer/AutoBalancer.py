f331c447fa16c3df15fb87fbba9157cc7d424688
Jakub Orsula <orsula.jakub@gmail.com>
2021-12-24 13:48:37 +0100

    autobalancer core

--- src/Strategies/AutoBalancer/AutoBalancer.py
+++ src/Strategies/AutoBalancer/AutoBalancer.py
@@ -4,14 +4,12 @@ import math
 from src.Brokerages.IBBrokerage import IBBrokerage
 from src.Executors.DummyExecutor import DummyExecutor
 from src.Executors.IBExecutor import IBExecutor
-from src.Strategies.AutoBalancer.AutoBalancerOptions import AutoBalancerOptions
-from src.Strategies.CashBurner.CashBurnerOptions import CashBurnerOptions
+from src.Strategies.AutoBalancer.AutoBalancerOptions import AutoBalancerOptions, TriggerStrategy, MitigationStrategy
 from src.Portfolios.Loaders.CombinedPortfolioLoader import CombinedPortfolioLoader
-from src.Portfolios.Loaders.IBPortfolioLoader import IBPortfolioLoader
-from src.Portfolios.Loaders.JSONPortfolioLoader import JSONPortfolioLoader
 from src.utils.jo_math import round_nearest
 from src.utils.security_data import get_current_price
-
+from tabulate import tabulate
+from statistics import mean
 
 def load_desired_values(src):
     fin = open(src)
@@ -45,8 +43,67 @@ if __name__ == "__main__":
     AutoBalancerOptions.print_options(opt)
     desired_portfolio = load_desired_values(opt.required_balances)
 
-    current_portfolio_value = 0
-    for name, exchangeLocation, requiredPercentage in desired_portfolio.items():
+    portf_loader = CombinedPortfolioLoader.create_comb_portf_loader(opt.external_portfolio, opt.ib_gateway_port,
+                                                                    opt.ib_gateway_readonly)
+
+    for position in desired_portfolio["balances"]:
+        name = position["name"]
+        ibName = position["ibName"]  # todo name should be the same as ibName hmm unify?
+        currency = position["currency"]
+        yahooName = position["yahooName"]
+        desiredPercentage = position["desiredPercentage"]
+        print(name, ibName, currency, yahooName, desiredPercentage)
+
+    current_portfolio_value = portf_loader.get_position_size(desired_portfolio["cash"]["currency"])
+    for position in desired_portfolio["balances"]:
+        yahooName = position["yahooName"]
+        name = position["name"]
+        position_size = portf_loader.get_position_size(name)
+        if position_size is None:
+            position_size = 0
+        position["currPrice"] = get_current_price(yahooName)
+        position["positionSize"] = position_size
+        current_portfolio_value += position["currPrice"] * position["positionSize"]
+
+    print(f'Current portfolio value {current_portfolio_value}')
+
+    for position in desired_portfolio["balances"]:
+        position["value"] = position["currPrice"] * position["positionSize"]  # added to dictionary just for the looks in tabulate, can be omitted
+        position["valuePercentage"] = position["value"] / current_portfolio_value * 100
+        position["desiredValue"] = (position["desiredPercentage"] / 100) * current_portfolio_value
+        position["deviation"] = position["desiredValue"] - position["value"]
+        position["deviationPercentage"] = abs(position["valuePercentage"] - position["desiredPercentage"])
+
+    print("Portfolio")
+    print(tabulate(list(desired_portfolio["balances"]), headers="keys"))
+
+    deviations = [position["deviationPercentage"] for position in desired_portfolio["balances"]]
+    highest_deviation = max(deviations)
+    lowest_deviation = min(deviations)
+    average_deviation = mean(deviations)
+
+    print(f'average deviation: {average_deviation}', f'lowest deviation: {lowest_deviation}', f'highest_deviation: {highest_deviation}', sep='\n')
+
+    if opt.trigger_strategy == TriggerStrategy.average and average_deviation > opt.trigger_deviation:
+        print(f'Tiggered by {opt.trigger_strategy.value} being {highest_deviation} which is over specified {opt.trigger_deviation}')
+    elif opt.trigger_strategy == TriggerStrategy.one_min and highest_deviation > opt.trigger_deviation:
+        print(f'Tiggered by {opt.trigger_strategy.value} being {highest_deviation} which is over specified {opt.trigger_deviation}') # todo abstract duplicity
+    elif opt.trigger_strategy == TriggerStrategy.all_min and lowest_deviation > opt.trigger_deviation:
+        print(f'Tiggered by {opt.trigger_strategy.value} being {lowest_deviation} which is over specified {opt.trigger_deviation}')  # todo abstract duplicity
+    else:
+        print(f'Trigger {opt.trigger_strategy.value} not fired. Not rebalancing.')
+
+    if opt.mitigation_strategy == MitigationStrategy.sell:
+        # todo add ability to specify different stock to sell than those in portfolio
+        # to sell, constraints (such as those required to comply with tax test)
+        # this should be done from separate json file
+        # todo create Seller strategy which will sell stocks
         pass
+
+    cash_to_burn = portf_loader.get_position_size(desired_portfolio["cash"]["currency"])
+    # todo call cash burner here (it needs some tuning and strategies to burn cash)
+
+
+
 
     # execute()
--- src/Strategies/AutoBalancer/AutoBalancerOptions.py
+++ src/Strategies/AutoBalancer/AutoBalancerOptions.py
@@ -1,4 +1,44 @@
 from src.Options.BaseOptions import BaseOptions
+import argparse
+import enum
+
+
+class EnumAction(argparse.Action):
+    """
+    Argparse action for handling Enums
+    """
+    def __init__(self, **kwargs):
+        # Pop off the type value
+        enum_type = kwargs.pop("type", None)
+
+        # Ensure an Enum subclass is provided
+        if enum_type is None:
+            raise ValueError("type must be assigned an Enum when using EnumAction")
+        if not issubclass(enum_type, enum.Enum):
+            raise TypeError("type must be an Enum when using EnumAction")
+
+        # Generate choices from the Enum
+        kwargs.setdefault("choices", tuple(e.value for e in enum_type))
+
+        super(EnumAction, self).__init__(**kwargs)
+
+        self._enum = enum_type
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Convert value back into an Enum
+        value = self._enum(values)
+        setattr(namespace, self.dest, value)
+
+
+class TriggerStrategy(enum.Enum):
+    one_min = "one-min"
+    all_min = "all-min"
+    average = "average"
+
+
+class MitigationStrategy(enum.Enum):
+    cash_only = "cash-only"  # no selling will take place and the difference will be settled just by cash
+    sell = "sell"  # stock will be sold to accumulate enough to rebalance portfolio
 
 
 class AutoBalancerOptions(BaseOptions):
@@ -7,4 +47,6 @@ class AutoBalancerOptions(BaseOptions):
     def parse():
         parser = super(AutoBalancerOptions, AutoBalancerOptions).parse()
         parser.add_argument('--required-balances', type=str, required=True, help="Location of json file with required balances specified")
+        parser.add_argument('--trigger-strategy', type=TriggerStrategy, action=EnumAction, required=True, help="What will trigger rebalance. See docs for possible values and meaning")
+        parser.add_argument('--mitigation-strategy', type=MitigationStrategy, action=EnumAction, required=True, help="How the portfolio will be rebalanced. See docs for possible values and meaning")
         return parser
